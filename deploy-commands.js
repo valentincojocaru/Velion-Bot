21d85fd30ae3efda00e82404cbe7e619dd3cba0e
Kode <85453793+valentincojocaru@users.noreply.github.com>
2026-04-17 18:48:13 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,16 +1,151 @@
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+
+const {
+  REST,
+  Routes,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} = require('discord.js');
 
 const commands = [
   new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Raspunde cu pong')
-    .toJSON(),
-];
+    .setDescription('Testeaza daca botul raspunde'),
+
+  new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Afiseaza lista comenzilor'),
+
+  new SlashCommandBuilder()
+    .setName('server')
+    .setDescription('Afiseaza informatii despre server'),
+
+  new SlashCommandBuilder()
+    .setName('verify')
+    .setDescription('Trimite panoul de verificare cu buton'),
+
+  new SlashCommandBuilder()
+    .setName('ticketpanel')
+    .setDescription('Trimite panoul de tickete cu categorii')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+
+  new SlashCommandBuilder()
+    .setName('say')
+    .setDescription('Botul trimite un mesaj')
+    .addStringOption(option =>
+      option
+        .setName('mesaj')
+        .setDescription('Mesajul pe care il va trimite botul')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+  new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Baneaza un membru')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Membrul')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('motiv')
+        .setDescription('Motivul banului')
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+  new SlashCommandBuilder()
+    .setName('kick')
+    .setDescription('Da kick unui membru')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Membrul')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('motiv')
+        .setDescription('Motivul kickului')
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+  new SlashCommandBuilder()
+    .setName('timeout')
+    .setDescription('Da timeout unui membru')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Membrul')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('minute')
+        .setDescription('Cate minute')
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(10080)
+    )
+    .addStringOption(option =>
+      option
+        .setName('motiv')
+        .setDescription('Motivul timeout-ului')
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+  new SlashCommandBuilder()
+    .setName('untimeout')
+    .setDescription('Scoate timeout-ul unui membru')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Membrul')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+
+  new SlashCommandBuilder()
+    .setName('purge')
+    .setDescription('Sterge mesaje')
+    .addIntegerOption(option =>
+      option
+        .setName('numar')
+        .setDescription('Numarul de mesaje de sters (1-100)')
+        .setRequired(true)
+        .setMinValue(1)
+        .setMaxValue(100)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+  new SlashCommandBuilder()
+    .setName('warn')
+    .setDescription('Da warn unui membru')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Membrul')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('motiv')
+        .setDescription('Motivul warn-ului')
+        .setRequired(false)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
+    console.log('Inregistrez comenzile...');
+
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
@@ -18,7 +153,8 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
       ),
       { body: commands }
     );
-    console.log('Comenzi adaugate');
+
+    console.log('Comenzile au fost inregistrate.');
   } catch (error) {
     console.error(error);
   }
